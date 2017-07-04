23453ca70a0bd10513c0e1d191550c0af3ed6271
cleonty <leonty@inbox.ru>
2017-07-04 10:40:08 +0500

    Arrow Up and Arrow Down keys work now

--- TwitterGeo.js
+++ TwitterGeo.js
@@ -1,19 +1,217 @@
+function Tweet(tweetObject, map, list) {
+  this.tweetObject = tweetObject;
+  this.map = map;
+  this.infoWindow;
+  this.list = list;
+  this.htmlContent = this.getHtmlContent();
+  this.marker = this.createMarker();
+  this.infoWindow = this.createInfoWindow();
+  this.listItem = this.createListItem();
+  this.onActivateListeners = [];
+}
+
+Tweet.prototype.getHtmlContent = function () {
+  var linkStart = this.tweetObject.text.lastIndexOf("https://");
+  var profileImageUrl = this.tweetObject.user.profile_image_url_https;
+  var formattedText;
+  if (linkStart >= 0) {
+    var textFragment = this.tweetObject.text.substr(0, linkStart);
+    var linkFragment = this.tweetObject.text.substr(linkStart);
+    formattedText = `${textFragment} <a target="_blank" href="${linkFragment}">${linkFragment}</a>`;
+  } else {
+    console.log('Not found URL in tweet', this.tweetObject.text);
+    formattedText = this.tweetObject.text;
+  }
+  return [
+    `<div class="media">`,
+    `  <div class="media-left">`,
+    `     <img src="${profileImageUrl}" class="media-object" style="width:48px">`,
+    `  </div>`,
+    `  <div class="media-body">`,
+    `    <h4 class="media-heading">${this.tweetObject.user.name}</h4>`,
+    `    <p>${formattedText}</p>`,
+    `  </div>`,
+    `</div>`
+  ].join('\n');
+};
+
+Tweet.prototype.createMarker = function () {
+  var marker = new google.maps.Marker({
+    position: {
+      lat: this.tweetObject.geo.coordinates[0],
+      lng: this.tweetObject.geo.coordinates[1]
+    },
+    map: this.map,
+    title: this.tweetObject.text
+  });
+  marker.addListener('click', this.activate.bind(this));
+  return marker;
+};
+
+Tweet.prototype.createInfoWindow = function () {
+  var infoWindow = new google.maps.InfoWindow({
+    content: this.htmlContent,
+    maxWidth: 320
+  });
+  return infoWindow;
+};
+
+Tweet.prototype.createListItem = function () {
+  var li = document.createElement('li');
+  li.className = 'list-group-item';
+  li.innerHTML = this.htmlContent;
+  li.addEventListener('click', this.activate.bind(this), false);
+  this.list.appendChild(li);
+  return li;
+};
+
+Tweet.prototype.openInfoWindow = function () {
+  this.infoWindow.open(this.map, this.marker);
+};
+
+Tweet.prototype.closeInfoWindow = function () {
+  this.infoWindow.close();
+};
+
+Tweet.prototype.activate = function () {
+  if (!this.active) {
+    this.openInfoWindow();
+    this.listItem.scrollIntoViewIfNeeded({
+      behavior: "smooth",
+      block: "start"
+    });
+    this.listItem.classList.add('list-group-item-info');
+    this.active = true;
+    this.notifyOnActivateListeners();
+  }
+};
+
+Tweet.prototype.deactivate = function () {
+  if (this.active) {
+    this.infoWindow.close();
+    this.listItem.classList.remove('list-group-item-info');
+    this.active = false;
+  }
+};
+
+Tweet.prototype.notifyOnActivateListeners = function () {
+  for (var i = 0; i < this.onActivateListeners.length; i++) {
+    this.onActivateListeners[i].call(null, this);
+  }
+};
+
+Tweet.prototype.addOnActivateListener = function (handler) {
+  if (typeof handler === 'function') {
+    this.onActivateListeners.push(handler);
+  }
+};
+
+Tweet.prototype.destroy = function () {
+  this.marker.setMap(null);
+};
+
+function TweetList(map, list) {
+  this.map = map;
+  this.list = list;
+  this.tweets = [];
+  this.activeTweet = null;
+  this.addKeyPressListener();
+}
+
+TweetList.prototype.setTweets = function (tweetObjects) {
+  this.clear();
+  for (var i = 0; i < tweetObjects.length; i++) {
+    this.addTweet(tweetObjects[i]);
+  }
+  if (this.tweets.length > 0) {
+    this.tweets[0].activate();
+  }
+};
+
+TweetList.prototype.addTweet = function (tweetObject) {
+  if (tweetObject.geo) {
+    var tweet = new Tweet(tweetObject, this.map, this.list);
+    this.tweets.push(tweet);
+    tweet.addOnActivateListener(this.onTweetActivated.bind(this));
+  } else {
+    console.log('No geo info in tweet', tweetObject);
+  }
+};
+
+TweetList.prototype.clear = function () {
+  for (var i = 0; i < this.tweets.length; i++) {
+    this.tweets[i].destroy();
+  }
+  this.tweets = [];
+  this.activeTweet = null;
+  while (this.list.firstChild) {
+    this.list.removeChild(this.list.firstChild);
+  }
+};
+
+TweetList.prototype.onTweetActivated = function (tweet) {
+  if (this.activeTweet !== tweet) {
+    var tweets = this.tweets;
+    for (var i = 0; i < tweets.length; i++) {
+      if (tweets[i] !== tweet) {
+        tweets[i].deactivate();
+      }
+    }
+    this.activeTweet = tweet;
+  }
+};
+
+TweetList.prototype.activateNextTweet = function () {
+  if (this.activeTweet) {
+    var nextTweetIndex = this.tweets.indexOf(this.activeTweet) + 1;
+    if (this.tweets[nextTweetIndex]) {
+      this.tweets[nextTweetIndex].activate();
+    } else {
+      this.tweets[0].activate();
+    }
+  }
+};
+
+TweetList.prototype.activatePreviousTweet = function () {
+  if (this.activeTweet) {
+    var nextTweetIndex = this.tweets.indexOf(this.activeTweet) - 1;
+    if (this.tweets[nextTweetIndex]) {
+      this.tweets[nextTweetIndex].activate();
+    } else {
+      this.tweets[this.tweets.length - 1].activate();
+    }
+  }
+};
+
+TweetList.prototype.keyPressed = function (e) {
+  console.log('key pressed', e);
+  if (e.code === 'ArrowUp') {
+    e.preventDefault();
+    e.stopPropagation();
+    this.activatePreviousTweet();
+  } else if (e.code === 'ArrowDown') {
+    e.preventDefault();
+    e.stopPropagation();
+    this.activateNextTweet();
+  }
+};
+
+TweetList.prototype.addKeyPressListener = function () {
+  document.addEventListener('keydown', this.keyPressed.bind(this));
+};
+
 function TwitterGeo(mapId, homeButtonId, listId) {
   this.mapId = mapId;
   this.homeButtonId = homeButtonId;
   this.listId = listId;
   this.list = document.getElementById(listId);
-  this.tweets = [];
-  this.markers = [];
-  this.currentInfoWindow = null;
-  this.currentMarker = null;
-  this.activeListItem = null;
   this.geoOptions = {
     enableHighAccuracy: true,
     maximumAge: 30000,
     timeout: 27000
   };
   this.map = this.createMap();
+  this.tweetList = new TweetList(this.map, this.list);
   this.addHomeButtonOnClickListener();
   this.goToCurrentPosition();
   this.watchMapPosition();
@@ -24,118 +222,10 @@ TwitterGeo.prototype.createMap = function () {
   var map = new google.maps.Map(document.getElementById(this.mapId), {
     zoom: 15,
     center: square1905,
-    mapTypeId: 'hybrid'
+    mapTypeId: 'hybrid',
+    keyboardShortcuts: false
   });
   return map;
-};
-
-TwitterGeo.prototype.removeMarkers = function () {
-  for (var i = 0; i < this.markers.length; i++) {
-    this.markers[i].setMap(null);
-  }
-  this.markers = [];
-};
-
-TwitterGeo.prototype.addMarker = function (tweet) {
-  var twitterGeo = this;
-  var marker = new google.maps.Marker({
-    position: {
-      lat: tweet.geo.coordinates[0],
-      lng: tweet.geo.coordinates[1]
-    },
-    map: this.map,
-    title: tweet.text
-  });
-  var linkStart = tweet.text.lastIndexOf("https://");
-  var profileImageUrl = tweet.user.profile_image_url_https;
-  var formattedText;
-  if (linkStart >= 0) {
-    var textFragment = tweet.text.substr(0, linkStart);
-    var linkFragment = tweet.text.substr(linkStart);
-    formattedText = `${textFragment} <a target="_blank" href="${linkFragment}">${linkFragment}</a>`;
-  } else {
-    console.log('Not found URL in tweet', tweet.text);
-    formattedText = tweet.text;
-  }
-  var content = [
-    `<div class="media">`,
-    `  <div class="media-left">`,
-    `     <img src="${profileImageUrl}" class="media-object" style="width:48px">`,
-    `  </div>`,
-    `  <div class="media-body">`,
-    `    <h4 class="media-heading">${tweet.user.name}</h4>`,
-    `    <p>${formattedText}</p>`,
-    `  </div>`,
-    `</div>`
-  ].join('\n');
-  var infoWindow = new google.maps.InfoWindow({
-    content: content,
-    maxWidth: 320
-  });
-  var listItem = this.addTweetToList(tweet, marker, infoWindow, content);
-  marker.addListener('click', function () {
-    twitterGeo.openInfoWindow(infoWindow, marker, listItem);
-    listItem.scrollIntoView({
-      behavior: "smooth",
-      block: "start"
-    });
-  });
-  this.markers.push(marker);
-  return marker;
-};
-
-TwitterGeo.prototype.addMarkers = function (tweets) {
-  for (var i = 0; i < tweets.length; i++) {
-    if (tweets[i].geo) {
-      var marker = this.addMarker(tweets[i]);
-    } else {
-      console.log('No geo information in tweet', tweets[i]);
-    }
-  }
-};
-
-TwitterGeo.prototype.openInfoWindow = function (infoWindow, marker, listItem) {
-  if (this.currentInfoWindow) {
-    this.currentInfoWindow.close();
-    this.currentInfoWindow = null;
-    this.currentMarker = null;
-  }
-  this.activateListItem(listItem);
-  infoWindow.open(this.map, marker);
-  this.currentInfoWindow = infoWindow;
-  this.currentMarker = marker;
-};
-
-TwitterGeo.prototype.clearList = function () {
-  var list = this.list;
-  if (list) {
-    while (list.firstChild) {
-      list.removeChild(list.firstChild);
-    }
-  }
-};
-
-TwitterGeo.prototype.addTweetToList = function (tweet, marker, infoWindow, textWithImage) {
-  var twitterGeo = this;
-  var list = this.list;
-  if (list) {
-    var li = document.createElement('li');
-    li.className = 'list-group-item';
-    li.innerHTML = textWithImage;
-    li.addEventListener('click', function (e) {
-      twitterGeo.openInfoWindow(infoWindow, marker, li);
-    });
-    list.appendChild(li);
-  }
-  return li;
-};
-
-TwitterGeo.prototype.activateListItem = function (listItem) {
-  if (this.activeListItem) {
-    this.activeListItem.classList.remove('active');
-  }
-  listItem.classList.add('active');
-  this.activeListItem = listItem;
 };
 
 TwitterGeo.prototype.reloadTweets = function (latitude, longitude) {
@@ -146,9 +236,7 @@ TwitterGeo.prototype.reloadTweets = function (latitude, longitude) {
   searchRequest.onload = function (e) {
     var jsonResponse = JSON.parse(searchRequest.responseText);
     var tweets = jsonResponse.statuses;
-    twitterGeo.removeMarkers();
-    twitterGeo.clearList();
-    twitterGeo.addMarkers(tweets);
+    twitterGeo.tweetList.setTweets(tweets);
   };
   searchRequest.onerror = function (e) {
     console.log(e);
